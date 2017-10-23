b86e9388cd2b4bf75cf07579b4493083279e2f47
Luiz Leite <feitosaleitelf@gmail.com>
2017-10-23 20:40:36 -0300

    Range sensor loop

--- range_sensor.py
+++ range_sensor.py
@@ -14,22 +14,24 @@ GPIO.output(TRIG, False)
 print "Waiting For Sensor To Settle"
 time.sleep(2)
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+while true:
 
-while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
+	GPIO.output(TRIG, True)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
+	while GPIO.input(ECHO)==0:
+	  pulse_start = time.time()
 
-pulse_duration = pulse_end - pulse_start
+	while GPIO.input(ECHO)==1:
+	  pulse_end = time.time()
 
-distance = pulse_duration * 17150
+	pulse_duration = pulse_end - pulse_start
 
-distance = round(distance, 2)
+	distance = pulse_duration * 17150
 
-print "Distance:",distance,"cm"
+	distance = round(distance, 2)
 
-GPIO.cleanup()
+	print "Distance:",distance,"cm"
+
+	GPIO.cleanup()
