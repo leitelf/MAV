519c0c4dd634e5dbdec8e190944108c69e1fadfb
Luiz Leite <feitosaleitelf@gmail.com>
2017-11-15 14:49:02 -0300

    Check RFID

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
 import RPi.GPIO as GPIO
 import time
 import MFRC522
 import signal
+import sys
+from coapthon.client.helperclient import HelperClient
+
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -42,98 +46,119 @@ def setAngle(angle, pin, pwm):
     pwm.ChangeDutyCycle(0)
 
 
-signal.signal(signal.SIGINT, stop_node)
+def main():
 
-MIFAREReader = MFRC522.MFRC522()
+	argc = len(sys.argv)
 
+	if argc != 3:
+		print("Esse script funciona com 2 argumentos: <host> <port>")
+		print("<host>: endereço IP do servidor")
+		print("<port>: porta do servidor")
+		exit(1)
 
-#RFID pinouts
+	host = sys.argv[1]
+	port = int(sys.argv[2])
 
-TRIG1 = 16 #pin 16, GPIO 23
-ECHO1 = 18 #pin 18, GPIO 24
+	signal.signal(signal.SIGINT, stop_node)
 
-TRIG2 = 8 #pin 8, GPIO 14
-ECHO2 = 10 #pin 10, GPIO 15
-
-MOTOR = 36 #pin 36, GPIO 16
-
-GPIO.setup(TRIG1, GPIO.OUT)
-GPIO.setup(TRIG2, GPIO.OUT)
-
-GPIO.setup(ECHO1, GPIO.IN)
-GPIO.setup(ECHO2, GPIO.IN)
-
-GPIO.setup(MOTOR, GPIO.OUT)
-
-PWM=GPIO.PWM(MOTOR, 50)
-PWM.start(0)
-
-setAngle(90, MOTOR, PWM)
-
-GPIO.output(TRIG1, False)
-GPIO.output(TRIG2, False)
-
-time.sleep(2)
+	MIFAREReader = MFRC522.MFRC522()
 
 
-print "Press Ctrl-C to stop."
+	#RFID pinouts
 
-allowed = False
+	TRIG1 = 16 #pin 16, GPIO 23
+	ECHO1 = 18 #pin 18, GPIO 24
 
-while nonstop:
+	TRIG2 = 8 #pin 8, GPIO 14
+	ECHO2 = 10 #pin 10, GPIO 15
 
-    #allowed = checkRFID(getRFID())
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+	MOTOR = 36 #pin 36, GPIO 16
 
-    if status == MIFAREReader.MI_OK:
-        print "Card detected"
+	GPIO.setup(TRIG1, GPIO.OUT)
+	GPIO.setup(TRIG2, GPIO.OUT)
 
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+	GPIO.setup(ECHO1, GPIO.IN)
+	GPIO.setup(ECHO2, GPIO.IN)
 
-    if status == MIFAREReader.MI_OK:
-        print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
-        # This is the default key for authentication
-        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+	GPIO.setup(MOTOR, GPIO.OUT)
 
-        # Select the scanned tag
-        MIFAREReader.MFRC522_SelectTag(uid)
+	PWM=GPIO.PWM(MOTOR, 50)
+	PWM.start(0)
 
-        # Authenticate
-        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+	setAngle(90, MOTOR, PWM)
 
-        # Check if authenticated
-        if status == MIFAREReader.MI_OK:
-            MIFAREReader.MFRC522_Read(8)
-            MIFAREReader.MFRC522_StopCrypto1()
-            #send uid to server
-            #server return if is valid
+	GPIO.output(TRIG1, False)
+	GPIO.output(TRIG2, False)
 
-            #for test
-            #consultar RFID
-            if uid[0] == 192:
-                allowed = True
-        else:
-            print "Authentication error"
+	time.sleep(2)
 
 
-    if allowed:
-        setAngle(0, MOTOR, PWM) #open
-        time.sleep(8)
-        setAngle(90, MOTOR, PWM) #close
-        allowed = False
+	print "Press Ctrl-C to stop."
 
-    distance1 = getDistance(TRIG1, ECHO1)
-    distance2 = getDistance(TRIG2, ECHO2)
+	allowed = False
 
-    if (distance1 < 20.0) and (distance2 < 20.0):
-        print "Car Waiting"
-        #if !allowed
-            #getPicture
-            #sendToServer
-        #tirar a foto
-        #enviar pro servidor
-        setAngle(0, MOTOR, PWM) #open
-        time.sleep(6)
-        setAngle(90, MOTOR, PWM) #close
-    else:
-        print "No car waiting"
+	while nonstop:
+
+	    #allowed = checkRFID(getRFID())
+	    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+	    if status == MIFAREReader.MI_OK:
+		print "Card detected"
+
+	    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+
+	    if status == MIFAREReader.MI_OK:
+		#print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
+		# This is the default key for authentication
+		key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+
+		# Select the scanned tag
+		MIFAREReader.MFRC522_SelectTag(uid)
+
+		# Authenticate
+		status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+
+		# Check if authenticated
+		if status == MIFAREReader.MI_OK:
+		    MIFAREReader.MFRC522_Read(8)
+		    MIFAREReader.MFRC522_StopCrypto1()
+		    #send uid to server
+		    #server return if is valid
+
+		    payload = str(uid[0])+"."str(uid[1])+"."str(uid[2])+"."str(uid[3])
+		    path = "coap://"+host+":"+port+"/rfid"
+		    response = client.put(path, payload)
+        	    print response.pretty_print()
+        	    client.stop()
+
+		    if response.payload == '1':
+		    	allowed = True
+		    #for test
+		    #consultar RFID
+		    #if uid[0] == 192:
+		    #    allowed = True
+		else:
+		    print "Authentication error"
+
+
+	    if allowed:
+		setAngle(0, MOTOR, PWM) #open
+		time.sleep(8)
+		setAngle(90, MOTOR, PWM) #close
+		allowed = False
+
+	    distance1 = getDistance(TRIG1, ECHO1)
+	    distance2 = getDistance(TRIG2, ECHO2)
+
+	    if (distance1 < 20.0) and (distance2 < 20.0):
+		print "Car Waiting"
+		#if !allowed
+		    #getPicture
+		    #sendToServer
+		#tirar a foto
+		#enviar pro servidor
+		setAngle(0, MOTOR, PWM) #open
+		time.sleep(6)
+		setAngle(90, MOTOR, PWM) #close
+	    #else:
+		#print "No car waiting"
