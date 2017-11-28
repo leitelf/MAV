8765eb2f19ed785abd728dbe724780e2d00ef0ed
Luiz Leite <feitosaleitelf@gmail.com>
2017-11-28 01:11:27 -0300

    Import to C

--- Pio/Debug/sources/gpio.d
+++
@@ -1,3 +0,0 @@
-sources/gpio.o: ../sources/gpio.c ../sources/../includes/gpio.h
-
-../sources/../includes/gpio.h:
--- Pio/Debug/utils/ultrasonic.d
+++
@@ -1,3 +0,0 @@
-utils/ultrasonic.o: ../utils/ultrasonic.c ../utils/../includes/gpio.h
-
-../utils/../includes/gpio.h:
